1b1f68cbea39549de346205b94f9943ac4cb853b
regusa6000 <60011505+regusa6000@users.noreply.github.com>
2021-08-25 10:35:33 +0200

    prueba consulta

--- lumen/app/Http/Controllers/ProductosController.php
+++ lumen/app/Http/Controllers/ProductosController.php
@@ -48,20 +48,29 @@ class ProductosController extends Controller{
                 $producto = DB::table('hg_product')
                                         ->leftJoin('hg_product_lang','hg_product.id_product','=','hg_product_lang.id_product')
                                         ->leftJoin('hg_image_shop','hg_image_shop.id_product','=','hg_product.id_product')
-                                        ->select('hg_product.id_product','hg_image_shop.id_image')
+                                        ->leftJoin('hg_product_attribute','hg_product_attribute.id_product','=','hg_product.id_product')
+                                        ->select('hg_product.id_product','hg_image_shop.id_image','hg_product_attribute.id_product_attribute')
                                         ->where('hg_product_lang.id_lang','=',1)
                                         ->where('hg_product.id_product','=',$id)
                                         //->groupBy(['hg_product.id_product','hg_product_attribute_combination.id_attribute','hg_image_shop.id_image','hg_product_attribute.reference'])
                                         ->get();
 
             // return response()->json($producto);
+
+
+
             $jsonResult = array();
+            /*Llenamos los campos recogidos, fuera del array para que no se repitan*/
+            $jsonResult[0]['id_product'] = $producto[0]->id_product;
+            $jsonResult[0]['id_image'] = $producto[0]->id_image;
+            $jsonResult[0]['id_product_attribute'] = $producto[0]->id_product_attribute;
+
             for ($a=0; $a < 1; $a++) {
 
-                if($producto[$a]->id_image == null){
+                if($producto[$a]->id_product_attribute != null){
                     //Cuando es nulo hacemos la consulta sobre la tabla "product_attribute"
-                    $jsonResult[$a]['id_product'] = $producto[$a]->id_product;
-                    $jsonResult[$a]['imagen'] = DB::table('hg_product')
+
+                    $jsonResult[$a]['attributes'] = DB::table('hg_product')
                                     ->leftJoin('hg_product_attribute','hg_product_attribute.id_product','=','hg_product.id_product')
                                     ->leftJoin('hg_product_attribute_combination','hg_product_attribute_combination.id_product_attribute','=','hg_product_attribute.id_product_attribute')
                                     ->leftJoin('hg_image_shop','hg_image_shop.id_product','=','hg_product.id_product')
@@ -70,27 +79,29 @@ class ProductosController extends Controller{
                                     ->where('hg_product.id_product','=',$id)
                                     ->orderBy('hg_product_attribute.id_product_attribute','ASC')
                                     ->get()
-                                    ->groupBy('hg_product.id_product','hg_product_attribute_combination.id_product_attribute','hg_image_shop.id_image');
+                                    ->groupBy('hg_product.id_product','hg_product_attribute_combination.id_product_attribute');
+
+                    return response()->json($jsonResult);
 
 
                 }else{
-
-                    $jsonResult[$a]['id_product'] = $producto[$a]->id_product;
-                    $jsonResult[$a]['imagen'] = DB::table('hg_product')
                     //Cuando no es nulo sobre la tabla "product_image"
-
+                    // $jsonResult[$a]['id_product'] = $producto[$a]->id_product;
+                    // $jsonResult[$a]['id_image'] = $producto[$a]->id_image;
+                    // $jsonResult[$a]['id_product_attribute'] = $producto[$a]->id_product_attribute;
+                    // $jsonResult[$a]['imagen'] = DB::table('hg_product')
+                    //                         ->leftJoin('hg_product_attribute','hg_product_attribute.id_product','=','hg_product.id_product')
+                    //                         ->leftJoin('hg_product_attribute_combination','hg_product_attribute_combination.id_product_attribute','=','hg_product_attribute.id_product_attribute')
+                    //                         ->leftJoin('hg_image_shop','hg_image_shop.id_product','=','hg_product.id_product')
+                    //                         ->select('hg_product_attribute.id_product_attribute','hg_product_attribute.reference','hg_product.reference as reference1','hg_image_shop.id_image')
+                    //                         ->where('hg_product.active','=',1)
+                    //                         ->where('hg_product.id_product','=',$id)
+                    //                         ->orderBy('hg_product_attribute.id_product_attribute','ASC')
+                    //                         ->get()
+                    //                         ->groupBy('hg_product.id_product','hg_product_attribute_combination.id_product_attribute','hg_product_attribute.id_product');
+                    $jsonResult[$a]['attributes'] = null;
+                    return response()->json($jsonResult);
                 }
-
-                $jsonResult[$a]['imagen'] = DB::table('hg_product')
-                                            ->leftJoin('hg_product_attribute','hg_product_attribute.id_product','=','hg_product.id_product')
-                                            ->leftJoin('hg_product_attribute_combination','hg_product_attribute_combination.id_product_attribute','=','hg_product_attribute.id_product_attribute')
-                                            ->leftJoin('hg_image_shop','hg_image_shop.id_product','=','hg_product.id_product')
-                                            ->select('hg_product_attribute.id_product_attribute','hg_product_attribute.reference','hg_product.reference as reference1','hg_image_shop.id_image')
-                                            ->where('hg_product.active','=',1)
-                                            ->where('hg_product.id_product','=',$id)
-                                            ->orderBy('hg_product_attribute.id_product_attribute','ASC')
-                                            ->get()
-                                            ->groupBy('hg_product.id_product','hg_product_attribute_combination.id_product_attribute','hg_image_shop.id_image');
 
 
                 /* Parte Buena de la consulta */
@@ -109,7 +120,7 @@ class ProductosController extends Controller{
             }
 
             // return response()->json($producto);
-            return response()->json($jsonResult);
+            // return response()->json($jsonResult);
 
         }
 
