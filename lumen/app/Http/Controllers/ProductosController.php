013fb722ed6281ecf3ee6a140bed0155f7ebb938
regusa6000 <60011505+regusa6000@users.noreply.github.com>
2021-08-26 09:27:53 +0200

    Borrar errores
    Errores

--- lumen/app/Http/Controllers/ProductosController.php
+++ lumen/app/Http/Controllers/ProductosController.php
@@ -43,36 +43,6 @@ class ProductosController extends Controller{
 
         public function principal(){
 
-            /*$producto = DB::table('hg_product')
-                                        ->leftJoin('hg_product_lang','hg_product.id_product','=','hg_product_lang.id_product')
-                                        ->leftJoin('hg_product_attribute','hg_product_attribute.id_product','=','hg_product.id_product')
-                                        ->leftJoin('hg_product_attribute_combination','hg_product_attribute_combination.id_product_attribute','=','hg_product_attribute.id_product_attribute')
-                                        ->leftJoin('hg_image_shop','hg_image_shop.id_product','=','hg_product.id_product')
-                                        ->select('hg_product.id_product','hg_product_lang.meta_description','hg_product_attribute.id_product_attribute','hg_image_shop.id_image','hg_product_attribute.reference','hg_product.reference as reference1')
-                                        ->where('hg_product_lang.id_lang','=',1)
-                                        ->where('hg_product.active','=',1)
-                                        ->orderBy('hg_image_shop.id_image','ASC')
-                                        // ->groupBy(['hg_product.id_product','hg_product_attribute_combination.id_attribute','hg_image_shop.id_image','hg_product_attribute.reference'])
-                                        ->get();
-
-
-            // $jsonResult = array();
-            // for ($a=0; $a < count($producto); $a++) {
-            //     $jsonResult[$a]['id_product'] = $producto[$a]->id_product;
-            //     $jsonResult[$a]['meta_description'] = $producto[$a]->meta_description;
-            //     $jsonResult[$a]['id_product_attribute'] = $producto[$a]->id_product_attribute;
-            //     $jsonResult[$a]['imagen'] = DB::table('hg_product')
-            //                                 ->leftJoin('hg_product_attribute','hg_product_attribute.id_product','=','hg_product.id_product')
-            //                                 ->leftJoin('hg_image_shop','hg_image_shop.id_product','=','hg_product.id_product')
-            //                                 ->select('hg_image_shop.id_image','hg_product_attribute.reference','hg_product.reference as reference1')
-            //                                 ->where('hg_product.active','=',1)
-            //                                 ->orderBy('hg_image_shop.id_image','ASC')
-            //                                 ->get();
-            // }
-
-            return response()->json($producto);
-            // return response()->json($jsonResult);*/
-
             $producto = DB::table('hg_image')
                         ->join('hg_product','hg_image.id_product','=','hg_product.id_product')
                         ->join('hg_product_lang','hg_product.id_product','=','hg_product_lang.id_product')
